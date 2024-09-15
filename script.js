ab3f7cbe3d800d9259e14144e2f437f0fd5a0a98
Milisource <natsukilover45+github@gmail.com>
2024-09-15 11:10:42 -0400

    Test

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+document.addEventListener("DOMContentLoaded", function() {
+    if (isMobileDevice()) {
+        document.getElementById('mobile-notification').style.display = 'block';
+    }
+});
